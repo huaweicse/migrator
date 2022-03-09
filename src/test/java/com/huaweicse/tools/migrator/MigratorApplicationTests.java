b499cf82544b9bf60c1d5cd69aa7242bec7a0794
david6969xin <1045319818@qq.com>
2022-03-09 16:59:32 +0800

    优化ModifyHSFConsumerAction测试逻辑，方便重复测试

--- src/test/java/com/huaweicse/tools/migrator/MigratorApplicationTests.java
+++ src/test/java/com/huaweicse/tools/migrator/MigratorApplicationTests.java
@@ -1,6 +1,7 @@
 package com.huaweicse.tools.migrator;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import org.apache.commons.io.IOUtils;
@@ -23,9 +24,13 @@ class MigratorApplicationTests {
 
   @Test
   void testModifyHSFConsumerAction() throws IOException {
+    // 测试文件修改是否正确
     String dirPath = BASE_PATH + "\\src\\test\\testfiles";
     modifyHSFConsumerAction.run(dirPath + "\\input", HSF_CONSUMER_PACKAGE_NAME, FEIGN_CLIENT_PACKAGE_NAME);
     Assert.assertTrue(IOUtils.contentEquals(new FileInputStream(dirPath + "\\input\\HSFConsumer.java"),
         new FileInputStream(dirPath + "\\output\\HSFConsumer.java")));
+    // 还原被修改的文件内容，方便下次测试
+    IOUtils.copy(new FileInputStream(dirPath + "\\originfiles\\HSFConsumer.java"),
+        new FileOutputStream(dirPath + "\\input\\HSFConsumer.java"));
   }
 }
