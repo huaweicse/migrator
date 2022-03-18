e835cf5487a5325c44c9c6c5476db68723a6fab3
liubao <bismy@qq.com>
2022-03-18 10:19:55 +0800

    修改换行和properties无序问题

---
+++ src/main/java/com/huaweicse/tools/migrator/OrderedWriteProperties.java
@@ -0,0 +1,18 @@
+package com.huaweicse.tools.migrator;
+
+import java.util.LinkedHashSet;
+import java.util.Properties;
+import java.util.Set;
+
+// ordered write properties
+public class OrderedWriteProperties extends Properties {
+  public OrderedWriteProperties(Properties other) {
+    super();
+    putAll(other);
+  }
+
+  @Override
+  public Set<Object> keySet() {
+    return new LinkedHashSet<>(super.keySet());
+  }
+}
