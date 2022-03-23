d5738fc23038e423c438c073a6dabf40a5ab2ea7
liubao <bismy@qq.com>
2022-03-23 09:17:34 +0800

    增加代码注释

--- src/main/java/com/huaweicse/tools/migrator/dubbo/ModifyDubboPomAction.java
+++ src/main/java/com/huaweicse/tools/migrator/dubbo/ModifyDubboPomAction.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Component;
 
 import com.huaweicse.tools.migrator.common.ModifyPomAction;
 
+/**
+ * 功能描述：
+ *   扫描目录下面的所有POM文件，删除Dubbo相关的依赖、插件，增加Spring Cloud相关的依赖、插件。
+ */
 @Component
 public class ModifyDubboPomAction extends ModifyPomAction {
   @Override
