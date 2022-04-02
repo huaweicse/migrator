891ca3d025922ef5159c9e4aa867df51a150593b
david6969xin <1045319818@qq.com>
2022-04-02 09:44:00 +0800

    修复由于系统换行符差异性所引起的测试用例失败

--- src/test/java/com/huaweicse/tools/migrator/Utils.java
+++ src/test/java/com/huaweicse/tools/migrator/Utils.java
@@ -1,7 +1,7 @@
 package com.huaweicse.tools.migrator;
 
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,9 +9,9 @@ import org.apache.commons.io.IOUtils;
 
 public class Utils {
   public static void assertFileContentEquals(String file1, String file2) throws Exception {
-    try (FileInputStream fis1 = new FileInputStream(file1)) {
-      try (FileInputStream fis2 = new FileInputStream(file2)) {
-        if (!IOUtils.contentEquals(fis1, fis2)) {
+    try (FileReader fis1 = new FileReader(file1)) {
+      try (FileReader fis2 = new FileReader(file2)) {
+        if (!IOUtils.contentEqualsIgnoreEOL(fis1, fis2)) {
           throw new AssertionError(String.format("file %s and %s not equal", file1, file2));
         }
       }
@@ -23,9 +23,9 @@ public class Utils {
     List<File> inputFiles = allFiles(list, inputFileDir);
     List<File> outputFiles = allFiles(list, outFileDir);
     for (int i = 0; i < inputFiles.size(); i++) {
-      try (FileInputStream fis1 = new FileInputStream(inputFiles.get(i))) {
-        try (FileInputStream fis2 = new FileInputStream(outputFiles.get(i))) {
-          if (!IOUtils.contentEquals(fis1, fis2)) {
+      try (FileReader fis1 = new FileReader(inputFiles.get(i))) {
+        try (FileReader fis2 = new FileReader(outputFiles.get(i))) {
+          if (!IOUtils.contentEqualsIgnoreEOL(fis1, fis2)) {
             throw new AssertionError(String.format("file %s and %s not equal", outFileDir, outFileDir));
           }
         }
